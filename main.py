ddc9775f38913c9a93a1d2711a580297c8162f00
Fabi <fabianflorete@yahoo.com>
2023-03-25 12:26:46 +0200

    done i think dar mai sunt cateva teste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 automat = dict()
 alfabet = []
 stariFinale = []
+stareInitiala = None
 """
 https://drive.google.com/file/d/1QBEbr0P_OpTgUGbsxT3-PgKvSQtkeLvd/view
 pg 23
@@ -12,16 +13,131 @@ for line in f.readlines():
     tranzitii = dict()
     parsedLinie = [(line[i], line[i + 1]) for i in range(1, len(line) - 1, 2)]
 
-    if line[0][-1] == "F":
+
+    if "iF" in line[0]:
+        stareInitiala = line[0][:-2]
+        stariFinale.append(line[0][:-2])
+        line[0] = line[0][:-2]
+    elif line[0][-1] == "F":
         stariFinale.append(line[0][:-1])
+        line[0] = line[0][:-1]
+    elif line[0][-1] == "i":
+        stareInitiala = line[0][:-1]
         line[0] = line[0][:-1]
 
     for tranzitie in parsedLinie:
         if tranzitie[0] not in alfabet:
             alfabet.append(tranzitie[0])
-            
+
     parsedLinie.append(('lambda', line[0]))
     automat.update({line[0]: dict(parsedLinie)})
 
-for elem in automat.items():
-    print(elem)
+f.close()
+
+separabile = {stare:dict() for stare in automat.keys()}
+
+lungime = 0
+cuvinteDeParcurs = alfabet.copy()
+"""s = ['lambda' for i in range(len(automat.keys()))]
+bkt(0)
+print(s)
+"""
+maxN = max([int(x) for x in automat])
+minN = min([int(x) for x in automat])
+
+for i in range(maxN, minN - 1, -1):
+    toAdd = []
+    for j in range(i - 1, minN - 1, - 1):
+        i = str(i)
+        j = str(j)
+        if (automat[i]['lambda'] in stariFinale and automat[j]['lambda'] not in stariFinale) or (automat[i]['lambda'] not in stariFinale and automat[j]['lambda'] in stariFinale):
+            toAdd.append((j, 'lambda'))
+    separabile[str(i)].update(dict(toAdd)) #interpretarea este: i este separabil de j prin separabile[i][j], daca aleg dictionarul altfel sunt duplicate si nu pot avea lambda la mai multe
+
+
+for _ in range(len(automat)):
+    for i in range(maxN, minN - 1, -1):
+        toAdd = []
+        for j in range(i - 1, minN - 1, -1):
+            i = str(i)
+            j = str(j)
+            for cuvant in cuvinteDeParcurs:
+                nod1 = i
+                nod2 = j
+                for litera in cuvant:
+                    nod1 = automat[nod1][litera]
+                    nod2 = automat[nod2][litera]
+                    #if nod1 == nod2: optimizare dar riscant
+                        #break
+                if(nod1 in stariFinale and nod2 not in stariFinale) or (nod1 not in stariFinale and nod2 in stariFinale):
+                    toAdd.append((j, cuvant))
+        for tuplu in toAdd:
+            if tuplu[0] not in separabile[str(i)]:
+                separabile[str(i)].update({tuplu[0]: tuplu[1]})
+    auxCuvinteDeParcurs = []
+    for cuvant in cuvinteDeParcurs:
+        for litera in alfabet:
+            auxCuvinteDeParcurs.append(cuvant + litera)
+    cuvinteDeParcurs = auxCuvinteDeParcurs.copy()
+
+#corectie pentru vid(unde nu pot fi separate => echivalente)
+for i in range(maxN, minN - 1, -1):
+    for j in range(i - 1, minN - 1, -1):
+        i = str(i)
+        j = str(j)
+        if j not in separabile[i].keys():
+            separabile[i][j] = 'vid'
+
+#print(separabile)
+
+newAutomat = dict()
+stariConcatenate = []
+for stare in separabile:
+    if 'vid' in separabile[stare].values():
+        for other in separabile[stare].keys():
+            if 'vid' in separabile[stare][other]:
+                newAutomat[other + "-" + stare] = {}
+                stariConcatenate.append(other)
+                stariConcatenate.append(stare)
+    else:
+        newAutomat[stare] = {}
+
+#elimin starile duplicate de genul 1, 1 2
+for duplicate in stariConcatenate:
+    try:
+        newAutomat.pop(duplicate)
+    except KeyError:
+        continue
+
+for newStare in newAutomat:
+    for stare in newStare.split('-'):
+        newAutomat[newStare] = automat[stare]
+
+#newNotations = {'1':'1 2', '2': '1 2'}
+newNotations = {}
+for newStare in newAutomat:
+    for stare in newStare.split('-'):
+        newNotations[stare] = newStare
+
+for newStare in newAutomat:
+    for tranzitie in newAutomat[newStare].keys():
+        try:
+            newAutomat[newStare][tranzitie] = newNotations[newAutomat[newStare][tranzitie]]
+        except KeyError: #TO BE TESTED!!!!!!!!!!!!
+            pass
+
+f = open("automat_minimal.txt", "w")
+
+newStareInitiala = newNotations[stareInitiala]
+newStariFinale = [newNotations[x] for x in stariFinale]
+
+for newStare in newAutomat:
+    to_write = newStare
+    if stareInitiala in newStare.split('-'):
+        to_write += 'i'
+    if newStare in newStariFinale:
+        to_write += 'F'
+    to_write += " "
+    for newTranzitie in newAutomat[newStare]:
+        to_write += newTranzitie + " " + newAutomat[newStare][newTranzitie] + " "
+    f.write(to_write + '\n')
