1fd3353e9a47658d2a41a19574276440168cd230
Fabi <fabianflorete@yahoo.com>
2023-04-15 16:06:52 +0300

    fully working me thinks

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ alfabet = []
 stariFinale = []
 stareInitiala = None
 """
-!!!trebuie eliminate starile inaccesibile (s ar putea sa fie dmictionarele goale) adica cv gen {'5':{}} este stare inaccesibila
 !!!trebuie sa ia si 3 stari de-o data ex q1q2q3
 !!!cand se contopesc cate 3 stari reiese din tranzitivitatea cu multimea vida in tabel
 
@@ -17,7 +16,6 @@ for line in f.readlines():
     line = line.strip('\n').split()
     tranzitii = dict()
     parsedLinie = [(line[i], line[i + 1]) for i in range(1, len(line) - 1, 2)]
-
 
     if "iF" in line[0]:
         stareInitiala = line[0][:-2]
@@ -38,8 +36,25 @@ for line in f.readlines():
     automat.update({line[0]: dict(parsedLinie)})
 
 f.close()
+#elimin starile inaccesibile
+stariDeParcurs = [stareInitiala]
+stariParcurse = []
+while len(stariDeParcurs) != 0:
+    stare = stariDeParcurs.pop(0)
+    stariParcurse.append(stare)
+    for litera in alfabet:
+        if automat[stare][litera] not in stariParcurse and automat[stare][litera] not in stariDeParcurs:
+            stariDeParcurs.append(automat[stare][litera])
+#print(stariParcurse)
+
+#print(automat)
+
+for stare in automat.copy():
+    if stare not in stariParcurse:
+        automat.pop(stare)
 
 separabile = {stare:dict() for stare in automat.keys()}
+#print(automat)
 
 
 lungime = 0
@@ -56,15 +71,21 @@ for i in range(maxN, minN - 1, -1):
     for j in range(i - 1, minN - 1, - 1):
         i = str(i)
         j = str(j)
+        if i not in automat.keys() or j not in automat.keys():
+            continue
         if (automat[i]['lambda'] in stariFinale and automat[j]['lambda'] not in stariFinale) or (automat[i]['lambda'] not in stariFinale and automat[j]['lambda'] in stariFinale):
             toAdd.append((j, 'lambda'))
-    separabile[str(i)].update(dict(toAdd)) #interpretarea este: i este separabil de j prin separabile[i][j], daca aleg dictionarul altfel sunt duplicate si nu pot avea lambda la mai multe
-
+    if i in automat.keys():
+        separabile[str(i)].update(dict(toAdd)) #interpretarea este: i este separabil de j prin separabile[i][j], daca aleg dictionarul altfel sunt duplicate si nu pot avea lambda la mai multe
 
 for _ in range(len(automat)):
     for i in range(maxN, minN - 1, -1):
+        if str(i) not in automat.keys():
+            continue
         toAdd = []
         for j in range(i - 1, minN - 1, -1):
+            if str(j) not in automat.keys():
+                continue
             i = str(i)
             j = str(j)
             for cuvant in cuvinteDeParcurs:
@@ -88,13 +109,15 @@ for _ in range(len(automat)):
 
 #corectie pentru vid(unde nu pot fi separate => echivalente)
 for i in range(maxN, minN - 1, -1):
+    if str(i) not in automat.keys():
+        continue
     for j in range(i - 1, minN - 1, -1):
+        if str(j) not in automat.keys():
+            continue
         i = str(i)
         j = str(j)
         if j not in separabile[i].keys():
             separabile[i][j] = 'vid'
-
-#print(separabile)
 
 newAutomat = dict()
 stariConcatenate = []
@@ -115,11 +138,31 @@ for duplicate in stariConcatenate:
     except KeyError:
         continue
 
+#folosesc tranzitivitatea pentru a verifica daca am cate 3 stari echivalente intr-un singur nod
+auxAutomat = dict()
+for newStare in newAutomat:
+    if '-' in newStare:
+        bigStare = set(newStare.split('-'))
+        for othernewStare in newAutomat:
+            if othernewStare == newStare:
+                continue
+            for stare in othernewStare.split('-'):
+                if stare in bigStare:
+                    for _ in othernewStare.split('-'):
+                        bigStare.add(_)
+                    break
+        auxAutomat["-".join(sorted(bigStare))] = dict()
+    else:
+        auxAutomat[newStare] = dict()
+newAutomat = auxAutomat
+
+#print(newAutomat)
+
 for newStare in newAutomat:
     for stare in newStare.split('-'):
         newAutomat[newStare] = automat[stare]
 
-#newNotations = {'1':'1 2', '2': '1 2'}
+#newNotations = {'1':'1-2', '2': '1-2'}
 newNotations = {}
 for newStare in newAutomat:
     for stare in newStare.split('-'):
@@ -149,3 +192,4 @@ for newStare in newAutomat:
     f.write(to_write + '\n')
 
 f.close()
+input("DONE")
